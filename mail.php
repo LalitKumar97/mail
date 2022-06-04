432e30e32bd2f177fdcaf7fa409a8b6d33d616ed
LalitKumar97 <lalitkumar9710888@gmail.com>
2022-06-04 14:09:42 +0530

    first

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message= $_POST['message'];
 $to = "lalitkumar9710888@gmail.com";
 $subject = "Mail From website local";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: lalitkumar971088@gmail.com" . "\r\n";
+$headers = "From: noreply@codeconia.com" . "\r\n";
 
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
