edc89d7252c39cd610e5b9849ef4dec7edc31661
LalitKumar97 <lalitkumar9710888@gmail.com>
2022-06-04 13:50:52 +0530

    first

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "lalitkumar9710888@gmail.com";
+$subject = "Mail From website local";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: lalitkumar971088@gmail.com" . "\r\n";
+
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
